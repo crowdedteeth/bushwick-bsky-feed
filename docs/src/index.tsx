3d618e24756a322f753d7426cd14b533f6a2b360
crowdedteeth <sacha@crowdedteeth.net>
2024-12-04 03:51:40 -0500

    Format

--- docs/src/index.tsx
+++ docs/src/index.tsx
@@ -1,12 +1,11 @@
-import {render} from 'preact';
+import { render } from 'preact';
 
 import bskyLogo from './assets/bsky.png';
 import firehose from './assets/firehose.svg';
-import {FeedIcon} from './components/feed_icon';
+import { FeedIcon } from './components/feed_icon';
 import './style.css';
 
-const FEED_LINK =
-  'https://bsky.app/profile/crowdedteeth.bsky.social/feed/bushwick';
+const FEED_LINK = 'https://bsky.app/profile/crowdedteeth.bsky.social/feed/bushwick';
 const USER_LINK = 'https://bsky.app/profile/crowdedteeth.bsky.social';
 
 /** */
@@ -15,19 +14,21 @@ export function App() {
     <div>
       <div class="bsky-logo-container">
         <a href={FEED_LINK}>
-          <img src={bskyLogo} height="48" />
+          <img
+            src={bskyLogo}
+            height="48"
+          />
         </a>
       </div>
-      <a target="_blank" href={FEED_LINK}>
+      <a
+        target="_blank"
+        href={FEED_LINK}>
         <div class="feed-info-container">
           <div class="feed-icon-container">
             <FeedIcon />
           </div>
           <div class="feed-description">
-            <h1>
-              🌆⋆.˚𖤓˚⟡&nbsp;&nbsp;&nbsp;Bushwick,
-              Brooklyn.&nbsp;&nbsp;&nbsp;⟡˚⏾˚.⋆🌃
-            </h1>
+            <h1>🌆⋆.˚𖤓˚⟡&nbsp;&nbsp;&nbsp;Bushwick, Brooklyn.&nbsp;&nbsp;&nbsp;⟡˚⏾˚.⋆🌃</h1>
             <p>Curated feed for Bushwick news, events, culture & more.</p>
             <p>#bushwickfeed</p>
           </div>
@@ -47,11 +48,13 @@ export function App() {
       </h2>
       <p>
         There appear to be some issues with the Bluesky firehose or my (see:{' '}
-        <a target="_blank" href={USER_LINK}>
+        <a
+          target="_blank"
+          href={USER_LINK}>
           @crowdedteeth
         </a>
-        ) server occasionally missing some posts. For now, the easiest thing may
-        be to delete the tag and post it again.
+        ) server occasionally missing some posts. For now, the easiest thing may be to delete the
+        tag and post it again.
       </p>
     </div>
   );
@@ -64,28 +67,28 @@ function Philosophy() {
     <div>
       <h1>Philosophy</h1>
       <p>
-        This feed is curated with an eye toward community building and
-        real-world engagement. For example, posts which promote local events,
-        community-owned businesses, or Bushwick-related news are of principal
-        interest. Secondarily, we aim to cultivate conversation among members of
-        our community by highlighting "hot"/"trending" posts pertaining to any
-        aspect of Bushwick's diverse culture.
+        This feed is curated with an eye toward community building and real-world engagement. For
+        example, posts which promote local events, community-owned businesses, or Bushwick-related
+        news are of principal interest. Secondarily, we aim to cultivate conversation among members
+        of our community by highlighting "hot"/"trending" posts pertaining to any aspect of
+        Bushwick's diverse culture.
         <br />
         <br />
-        Practically, this means that posts are often added ad hoc by replying
-        with the&nbsp;<code>#bushwickfeed</code>&nbsp;tag when they meet the
-        criteria. Subscribers of the feed are encouraged to use this tag to
-        promote their own posts as well as applicable content they encounter in
-        the wild!
+        Practically, this means that posts are often added ad hoc by replying with the&nbsp;
+        <code>#bushwickfeed</code>&nbsp;tag when they meet the criteria. Subscribers of the feed are
+        encouraged to use this tag to promote their own posts as well as applicable content they
+        encounter in the wild!
         <br />
         <br />
-        Certain fixtures of the Bushwick community (e.g., Bushwick Daily) are
-        allowlisted to include all their posts in the feed. If you have a
-        suggestion for a new allowlisted account, please suggest it below!
+        Certain fixtures of the Bushwick community (e.g., Bushwick Daily) are allowlisted to include
+        all their posts in the feed. If you have a suggestion for a new allowlisted account, please
+        suggest it below!
         <br />
         <br />
         Do reach out to{' '}
-        <a target="_blank" href={USER_LINK}>
+        <a
+          target="_blank"
+          href={USER_LINK}>
           @crowdedteeth
         </a>{' '}
         with any suggestions or concerns :)
@@ -99,8 +102,8 @@ function Algorithm() {
     <div>
       <h1>Algorithm</h1>
       <p>
-        In the interest of clarity and transparency, the algorithm used by this
-        feed's generator is reproduced diagrammatically below.
+        In the interest of clarity and transparency, the algorithm used by this feed's generator is
+        reproduced diagrammatically below.
         <br />
         <br />
         The source code for this feed is also available on GitHub!&nbsp;
@@ -117,9 +120,13 @@ function Algorithm() {
         <br />
       </p>
       <div class="algo-viz-container">
-        <img id="firehose" src={firehose} height="48" />
+        <img
+          id="firehose"
+          src={firehose}
+          height="48"
+        />
         <div class="algo-linefeed">
-          <code style={{background: '#db7d09'}}>WIP</code>&nbsp;
+          <code style={{ background: '#db7d09' }}>WIP</code>&nbsp;
           <code>Accept</code>
           &nbsp;any&nbsp;
           <a>post</a>
@@ -138,14 +145,17 @@ function Algorithm() {
           <code>#bushwickfeed</code>
         </div>
         <div class="algo-linefeed">
-          <code style={{background: '#db7d09'}}>WIP</code>&nbsp;
+          <code style={{ background: '#db7d09' }}>WIP</code>&nbsp;
           <code>Accept</code>
           &nbsp;any&nbsp;
           <a>post</a>
           &nbsp;with&nbsp;
-          <i style={{opacity: '0.6'}}>[TBD for "hot"/"trending" criteria]</i>
+          <i style={{ opacity: '0.6' }}>[TBD for "hot"/"trending" criteria]</i>
         </div>
-        <div class="algo-linefeed" style={{visibility: 'hidden'}} />
+        <div
+          class="algo-linefeed"
+          style={{ visibility: 'hidden' }}
+        />
         <div class="feed-container">
           <FeedIcon />
         </div>
@@ -155,14 +165,14 @@ function Algorithm() {
 }
 
 function UserCardList(props) {
-  const {users} = props;
+  const { users } = props;
 
   return (
     <div class="user-card-list">
       {users.map((user, index) => (
         <UserCard
           user={user}
-          style={{left: `${48 * (users.length - index - 1)}px`}}
+          style={{ left: `${48 * (users.length - index - 1)}px` }}
         />
       ))}
     </div>
@@ -171,7 +181,7 @@ function UserCardList(props) {
 
 function UserCard(props) {
   const {
-    user: {name, icon, handle},
+    user: { name, icon, handle },
     style,
   } = props;
 
